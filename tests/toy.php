f863c46b57e7a244d29577f2b99c7e83499a05f3
Ivan Enderlin <ivan.enderlin@hoa-project.net>
2018-11-18 11:55:14 -0500

    fix(tests) Fix a path.

--- tests/toy.php
+++ tests/toy.php
@@ -2,5 +2,5 @@
 
 require_once dirname(__DIR__) . '/lib/WASM.php';
 
-$instance = new WASM\Instance('./tests/toy.wasm');
+$instance = new WASM\Instance(__DIR__ . '/toy.wasm');
 $instance->sum(5, 37);

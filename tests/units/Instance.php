2ca3e6ddfde67d976106d119ba06acb9800c0951
Ivan Enderlin <ivan.enderlin@hoa-project.net>
2018-11-20 13:20:44 +0100

    test(lib,php) Add test cases for `Instance` and `ArgumentsBuilder`.

--- tests/units/Instance.php
+++ tests/units/Instance.php
@@ -9,6 +9,8 @@ use WASM\Tests\Suite;
 
 class Instance extends Suite
 {
+    const FILE_PATH = __DIR__ . '/tests.wasm';
+
     public function test_constructor_invalid_path()
     {
         $this
@@ -20,5 +22,137 @@ class Instance extends Suite
             )
                 ->isInstanceOf(RuntimeException::class)
                 ->hasMessage("File path to WASM binary `$filePath` does not exist.");
+    }
+
+    public function test_basic_sum()
+    {
+        $this
+            ->given($wasmInstance = new SUT(self::FILE_PATH))
+            ->when($result = $wasmInstance->sum(1, 2))
+            ->then
+                ->integer($result)
+                    ->isEqualTo(3);
+    }
+
+    public function test_call_undefined_function()
+    {
+        $this
+            ->given($wasmInstance = new SUT(self::FILE_PATH))
+            ->exception(
+                function () use ($wasmInstance) {
+                    $wasmInstance->ƒ();
+                }
+            )
+                ->isInstanceOf(InvocationException::class)
+                ->hasMessage('Function `ƒ` does not exist.');
+    }
+
+    public function test_call_missing_arguments()
+    {
+        $this
+            ->given($wasmInstance = new SUT(self::FILE_PATH))
+            ->exception(
+                function () use ($wasmInstance) {
+                    $wasmInstance->sum(1);
+                }
+            )
+                ->isInstanceOf(InvocationException::class)
+                ->hasMessage(
+                    'Missing 1 argument(s) when calling `sum`: ' .
+                    'Expect 2 arguments, given 1.'
+                );
+    }
+
+    public function test_call_extra_arguments()
+    {
+        $this
+            ->given($wasmInstance = new SUT(self::FILE_PATH))
+            ->exception(
+                function () use ($wasmInstance) {
+                    $wasmInstance->sum(1, 2, 3);
+                }
+            )
+                ->isInstanceOf(InvocationException::class)
+                ->hasMessage(
+                    'Given 1 extra argument(s) when calling `sum`: ' .
+                    'Expect 2 arguments, given 3.'
+                );
+    }
+
+    public function test_call_arity_0()
+    {
+        $this
+            ->given($wasmInstance = new SUT(self::FILE_PATH))
+            ->when($result = $wasmInstance->arity_0())
+            ->then
+                ->integer($result)
+                    ->isEqualTo(42);
+    }
+
+    public function test_call_return_void()
+    {
+        $this
+            ->given($wasmInstance = new SUT(self::FILE_PATH))
+            ->when($result = $wasmInstance->void())
+            ->then
+                ->variable($result)
+                    ->isNull();
+    }
+
+    public function test_call_i32_i32()
+    {
+        $this
+            ->given($wasmInstance = new SUT(self::FILE_PATH))
+            ->when($result = $wasmInstance->i32_i32(7))
+            ->then
+                ->integer($result)
+                    ->isEqualTo(7);
+    }
+
+    public function test_call_i64_i64()
+    {
+        $this
+            ->given($wasmInstance = new SUT(self::FILE_PATH))
+            ->when($result = $wasmInstance->i64_i64(7))
+            ->then
+                ->integer($result)
+                    ->isEqualTo(7);
+    }
+
+    public function test_call_f32_f32()
+    {
+        $this
+            ->given($wasmInstance = new SUT(self::FILE_PATH))
+            ->when($result = $wasmInstance->f32_f32(7.42))
+            ->then
+                ->float($result)
+                    ->isNearlyEqualTo(7.42, 64);
+    }
+
+    public function test_call_f64_f64()
+    {
+        $this
+            ->given($wasmInstance = new SUT(self::FILE_PATH))
+            ->when($result = $wasmInstance->f64_f64(7.42))
+            ->then
+                ->float($result)
+                    ->isNearlyEqualTo(7.42, 64);
+    }
+
+    public function test_call_i32_i64_f32_f64_f64()
+    {
+        $this
+            ->given($wasmInstance = new SUT(self::FILE_PATH))
+            ->when(
+                $result = $wasmInstance->i32_i64_f32_f64_f64(
+                    1,
+                    2,
+                    3.4,
+                    5.6
+                )
+            )
+            ->then
+                ->float($result)
+                    ->isNearlyEqualTo(1 + 2 + 3.4 + 5.6, 64);
     }
 }
---
+++ tests/units/tests.rs
@@ -0,0 +1,37 @@
+#[no_mangle]
+pub extern "C" fn sum(x: i32, y: i32) -> i32 {
+    x + y
+}
+
+#[no_mangle]
+pub extern "C" fn arity_0() -> i32 {
+    42
+}
+
+#[no_mangle]
+pub extern "C" fn void() {}
+
+#[no_mangle]
+pub extern "C" fn i32_i32(x: i32) -> i32 {
+    x
+}
+
+#[no_mangle]
+pub extern "C" fn i64_i64(x: i64) -> i64 {
+    x
+}
+
+#[no_mangle]
+pub extern "C" fn f32_f32(x: f32) -> f32 {
+    x
+}
+
+#[no_mangle]
+pub extern "C" fn f64_f64(x: f64) -> f64 {
+    x
+}
+
+#[no_mangle]
+pub extern "C" fn i32_i64_f32_f64_f64(a: i32, b: i64, c: f32, d: f64) -> f64 {
+    a as f64 + b as f64 + c as f64 + d
+}

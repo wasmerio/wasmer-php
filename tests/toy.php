e612dc640c26963f69f7a043d44ea78cbb1f958a
Ivan Enderlin <ivan.enderlin@hoa-project.net>
2018-11-20 09:49:05 +0100

    test(lib) Add one test case.

--- tests/toy.php
+++ tests/toy.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once dirname(__DIR__) . '/lib/WASM.php';
+require_once dirname(__DIR__) . '/vendor/autoload.php';
 
 $instance = new WASM\Instance(__DIR__ . '/toy.wasm');
 $result = $instance->sum(5, 37);
--- tests/units/Instance.php
+++ tests/units/Instance.php
@@ -2,12 +2,23 @@
 
 namespace WASM\Tests\Units;
 
+use RuntimeException;
+use WASM\Instance as SUT;
+use WASM\InvocationException;
 use WASM\Tests\Suite;
 
 class Instance extends Suite
 {
-    public function test_foo()
+    public function test_constructor_invalid_path()
     {
-        $this->integer(1)->isEqualTo(1);
+        $this
+            ->given($filePath = '/foo/bar')
+            ->exception(
+                function () use ($filePath) {
+                    new SUT($filePath);
+                }
+            )
+                ->isInstanceOf(RuntimeException::class)
+                ->hasMessage("File path to WASM binary `$filePath` does not exist.");
     }
 }

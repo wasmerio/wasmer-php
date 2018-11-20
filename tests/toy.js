f27cddb3916eaf5f3e674a61b986fdf378604be8
Ivan Enderlin <ivan.enderlin@hoa-project.net>
2018-11-20 13:48:57 +0100

    chore(test) Cleanup.

--- tests/toy.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env node
-
-const fs = require('fs');
-
-WebAssembly
-    .instantiate(fs.readFileSync('toy.wasm'), {})
-    .then(result => result.instance.exports.sum(1, 2))
-    .then(console.log);

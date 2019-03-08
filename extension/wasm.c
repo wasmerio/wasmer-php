a0d0edb7311d7552ee3197f984ea1ab2a8ba8eaf
Ivan Enderlin <ivan.enderlin@hoa-project.net>
2019-03-08 14:16:03 +0100

    feat(extension) Restore the `wasm_function_arguments_builder_add_i64`.
    Previously named `wasm_invoke_arguments_builder_add_i64`.

--- extension/wasm.c
+++ extension/wasm.c
@@ -386,25 +386,35 @@ PHP_FUNCTION(wasm_function_arguments_builder_add_i32)
     RETURN_TRUE
 }
 
-///**
-// * `wasm_invoke_arguments_builder_add_i64`.
-// */
-//
-//PHP_FUNCTION(wasm_invoke_arguments_builder_add_i64)
-//{
-//    zval *wasm_arguments_builder_resource;
-//    zend_long number;
-//
-//    if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "rl", &wasm_arguments_builder_resource, &number) == FAILURE) {
-//        return;
-//    }
-//
-//    Vec_RuntimeValue *wasm_arguments_builder = wasm_arguments_builder_from_resource(Z_RES_P(wasm_arguments_builder_resource));
-//    wasm_invoke_arguments_builder_add_i64(wasm_arguments_builder, (int64_t) number);
-//
-//    RETURN_TRUE
-//}
-//
+/**
+ * `wasm_function_arguments_builder_add_i64`.
+ */
+
+PHP_FUNCTION(wasm_function_arguments_builder_add_i64)
+{
+    zval *wasm_function_arguments_builder_resource;
+    zend_long number;
+
+    if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "rl", &wasm_function_arguments_builder_resource, &number) == FAILURE) {
+        return;
+    }
+
+    wasm_value_array_t *wasm_function_arguments = wasm_function_arguments_builder_from_resource(Z_RES_P(wasm_function_arguments_builder_resource));
+
+    if (wasm_function_arguments->length == (wasm_function_arguments->last_used_index + 1)) {
+        RETURN_FALSE
+    }
+
+    wasmer_value_t argument;
+    argument.tag = WASM_I64;
+    argument.value.I64 = (int64_t) number;
+
+    wasm_function_arguments->values[wasm_function_arguments->last_used_index + 1] = argument;
+    wasm_function_arguments->last_used_index += 1;
+
+    RETURN_TRUE
+}
+
 ///**
 // * `wasm_invoke_arguments_builder_add_f32`.
 // */
@@ -560,11 +570,11 @@ ZEND_BEGIN_ARG_INFO(arginfo_wasm_function_arguments_builder_add_i32, 0)
     ZEND_ARG_INFO(0, argument_value_as_i32)
 ZEND_END_ARG_INFO()
 
-//ZEND_BEGIN_ARG_INFO(arginfo_wasm_invoke_arguments_builder_add_i64, 0)
-//    ZEND_ARG_INFO(1, wasm_invoke_arguments_builder)
-//    ZEND_ARG_INFO(0, argument_value)
-//ZEND_END_ARG_INFO()
-//
+ZEND_BEGIN_ARG_INFO(arginfo_wasm_function_arguments_builder_add_i64, 0)
+    ZEND_ARG_INFO(1, wasm_function_arguments_builder)
+    ZEND_ARG_INFO(0, argument_value_as_i64)
+ZEND_END_ARG_INFO()
+
 //ZEND_BEGIN_ARG_INFO(arginfo_wasm_invoke_arguments_builder_add_f32, 0)
 //    ZEND_ARG_INFO(1, wasm_invoke_arguments_builder)
 //    ZEND_ARG_INFO(0, argument_value)
@@ -589,7 +599,7 @@ static const zend_function_entry wasm_functions[] = {
     //PHP_FE(wasm_get_function_signature,				arginfo_wasm_get_function_signature)
     PHP_FE(wasm_function_arguments_builder,			arginfo_wasm_function_arguments_builder)
     PHP_FE(wasm_function_arguments_builder_add_i32,	arginfo_wasm_function_arguments_builder_add_i32)
-    //PHP_FE(wasm_invoke_arguments_builder_add_i64,	arginfo_wasm_invoke_arguments_builder_add_i64)
+    PHP_FE(wasm_function_arguments_builder_add_i64,	arginfo_wasm_function_arguments_builder_add_i64)
     //PHP_FE(wasm_invoke_arguments_builder_add_f32,	arginfo_wasm_invoke_arguments_builder_add_f32)
     //PHP_FE(wasm_invoke_arguments_builder_add_f64,	arginfo_wasm_invoke_arguments_builder_add_f64)
     //PHP_FE(wasm_invoke_function,					arginfo_wasm_invoke_function)

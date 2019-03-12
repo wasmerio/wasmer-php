d70eb93b488b322d9387148b116e421a4eccf84b
Ivan Enderlin <ivan.enderlin@hoa-project.net>
2019-03-12 17:10:55 +0100

    feat(lib) Update namespace and adapt to the new API.
    The new namespace is `Wasm` instead of `WASM`.
    The library adopts the new API provided by the extension.

--- lib/Instance.php
+++ lib/Instance.php
@@ -2,7 +2,7 @@
 
 declare(strict_types = 1);
 
-namespace WASM;
+namespace Wasm;
 
 use Closure;
 use ReflectionObject;
@@ -12,128 +12,21 @@ final class Instance
 {
     private $filePath;
     private $wasmRuntime;
-    private $wasmBinary;
     private $importedFunctions = [];
 
-    public function __construct(string $filePath, array $importedFunctions = [])
+    public function __construct(string $filePath)
     {
         if (false === file_exists($filePath)) {
             throw new RuntimeException("File path to WASM binary `$filePath` does not exist.");
         }
 
-        $this->buildImportedFunctions($importedFunctions);
-
         $this->filePath = $filePath;
-        $this->wasmBinary = wasm_read_binary($this->filePath);
-        $this->wasmRuntime = wasm_new_runtime();
+        $wasmBytes = wasm_read_bytes($this->filePath);
 
-        foreach ($this->importedFunctions as $importedFunctionName => $importedFunction) {
-            wasm_runtime_add_function(
-                $this->wasmRuntime,
-                $importedFunction['index'],
-                $importedFunctionName,
-                $importedFunction['signature'],
-                $importedFunction['implementation']
-            );
-        }
-
-        $this->wasmInstance = wasm_new_instance(
-            $this->filePath,
-            $this->wasmBinary,
-            $this->wasmRuntime
-        );
+        $this->wasmInstance = wasm_new_instance($wasmBytes);
 
         if (null === $this->wasmInstance) {
             throw new RuntimeException("An error happened while instanciating the module `$filePath`.");
-        }
-    }
-
-    private function buildImportedFunctions(array $importedFunctions) {
-        $index = 0;
-
-        foreach ($importedFunctions as $functionName => $functionImplementation) {
-            if (!($functionImplementation instanceof Closure)) {
-                throw new RuntimeException("Import function `$functionName` must be a closure.");
-            }
-
-            $reflection = new ReflectionObject($functionImplementation);
-            $invoke = $reflection->getMethod('__invoke');
-            $inputs = $invoke->getParameters();
-
-            $signature = [];
-
-            foreach ($inputs as $input) {
-                if (true !== $input->hasType()) {
-                    throw new RuntimeException(
-                        "Argument `\${$input->getName()}` " .
-                        "of the imported function `$functionName` must have a type."
-                    );
-                }
-
-                $type = $input->getType();
-
-                if (true === $type->allowsNull()) {
-                    throw new RuntimeException(
-                        "Argument `\${$input->getName()}` " .
-                        "of the imported function `$functionName` must _not_ have a nullable type."
-                    );
-                }
-
-                switch ($type . '') {
-                    case 'int':
-                    case 'double':
-                        $signature[] = SIGNATURE_TYPE_I32;
-                        break;
-
-                    case 'float':
-                        $signature[] = SIGNATURE_TYPE_F32;
-                        break;
-
-                    default:
-                        throw new RuntimeException(
-                            "Argument `\${$input->getName()}` " .
-                            "of the imported function `$functionName` has an invalid type: " .
-                            "Only `int`, `double`, or `float` is supported."
-                        );
-                }
-            }
-
-            $output = $invoke->getReturnType();
-
-            if (null === $output) {
-                throw new RuntimeException(
-                    "The imported function `$functionName` must have a return type."
-                );
-            }
-
-            if (true === $output->allowsNull()) {
-                throw new RuntimeException(
-                    "The imported function `$functionName` must _not_ have a nullable return type."
-                );
-            }
-
-            switch ($output . '') {
-                case 'int':
-                case 'double':
-                    $signature[] = SIGNATURE_TYPE_I32;
-                    break;
-
-                case 'float':
-                    $signature[] = SIGNATURE_TYPE_F32;
-                    break;
-
-                default:
-                    throw new RuntimeException(
-                        "The imported function `$functionName` has an invalid return type: " .
-                        "Only `int`, `double`, or `float` is supported."
-                    );
-            }
-
-            $this->importedFunctions[$functionName] = [
-                'index' => $index++,
-                'signature' => $signature,
-                'implementation' => $functionImplementation,
-            ];
         }
     }
 
@@ -165,45 +58,45 @@ final class Instance
             );
         }
 
-        $argumentsBuilder = new ArgumentsBuilder();
+        $wasmArguments = [];
 
         foreach ($arguments as $i => $argument) {
             $s = $i + 1;
 
             switch ($signature[$i]) {
-                case SIGNATURE_TYPE_I32:
+                case I32:
                     if (!is_int($argument)) {
                         throw new InvocationException("Argument #$s of `$name` must be a `i32` (integer).");
                     }
 
-                    $argumentsBuilder->addI32($argument);
+                    $wasmArguments[] = wasm_value(I32, $argument);
 
                     break;
 
-                case SIGNATURE_TYPE_I64:
+                case I64:
                     if (!is_int($argument)) {
                         throw new InvocationException("Argument #$s of `$name` must be a `i64` (integer).");
                     }
 
-                    $argumentsBuilder->addI64($argument);
+                    $wasmArguments[] = wasm_value(I64, $argument);
 
                     break;
 
-                case SIGNATURE_TYPE_F32:
+                case F32:
                     if (!is_float($argument)) {
                         throw new InvocationException("Argument #$s of `$name` must be a `f32` (float).");
                     }
 
-                    $argumentsBuilder->addF32($argument);
+                    $wasmArguments[] = wasm_value(F32, $argument);
 
                     break;
 
-                case SIGNATURE_TYPE_F64:
+                case F64:
                     if (!is_float($argument)) {
                         throw new InvocationException("Argument #$s of `$name` must be a `f64` (float).");
                     }
 
-                    $argumentsBuilder->addF64($argument);
+                    $wasmArguments[] = wasm_value(F64, $argument);
 
                     break;
 
@@ -212,12 +105,10 @@ final class Instance
             }
         }
 
-        $argumentsBuilderResource = $argumentsBuilder->intoResource();
         $result = wasm_invoke_function(
             $this->wasmInstance,
             $name,
-            $argumentsBuilderResource,
-            $this->wasmRuntime
+            $wasmArguments
         );
 
         if (false === $result) {

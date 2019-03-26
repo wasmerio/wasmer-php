56e0686adebe64db54eba10ba93b8482fcd66c9a
Ivan Enderlin <ivan.enderlin@hoa-project.net>
2019-03-26 14:36:15 +0100

    test(lib) Add test suite for `Wasm\TypedArray`.

---
+++ tests/units/TypedArray.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Wasm\Tests\Units;
+
+use Wasm as LUT;
+use WasmArrayBuffer;
+use WasmInt16Array;
+use WasmInt32Array;
+use WasmInt8Array;
+use WasmUint16Array;
+use WasmUint32Array;
+use WasmUint8Array;
+use Wasm\Tests\Suite;
+
+class TypedArray extends Suite
+{
+    /**
+     * @dataProvider typed_arrays
+     */
+    public function test_typed_arrays(string $typedArrayName, string $wasmTypedArrayName)
+    {
+        $this
+            ->given($wasmArrayBuffer = new WasmArrayBuffer(8))
+            ->when($result = new $typedArrayName($wasmArrayBuffer))
+            ->then
+                ->object($result)
+                    ->isInstanceOf($wasmTypedArrayName)
+                    ->isInstanceof(LUT\TypedArray::class);
+    }
+
+    protected function typed_arrays()
+    {
+        yield [LUT\Int8Array::class,   WasmInt8Array::class];
+        yield [LUT\Int16Array::class,  WasmInt16Array::class];
+        yield [LUT\Int32Array::class,  WasmInt32Array::class];
+        yield [LUT\Uint8Array::class,  WasmUint8Array::class];
+        yield [LUT\Uint16Array::class, WasmUint16Array::class];
+        yield [LUT\Uint32Array::class, WasmUint32Array::class];
+    }
+}
